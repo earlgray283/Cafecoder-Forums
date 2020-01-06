50f3a8ab1627d7ddbf84d508ba0ecb20bb7956b2
earlgray283 <earlgray283@gmail.com>
2020-01-06 22:56:56 +0900

    add install.sql

--- template/header.php
+++ template/header.php
@@ -5,8 +5,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+<!-- Google Font -->
 <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP&display=swap" rel="stylesheet">
-
+<!-- Favicon -->
+<link rel="shortcut icon" href="https://2.bp.blogspot.com/-2PgVP0iOkHY/USNoKzJtQlI/AAAAAAAANSI/1xMd2jtWmPA/s1600/cafe_tea.png">
 <style>
     body {
         font-family: 'Noto Sans JP', sans-serif;
