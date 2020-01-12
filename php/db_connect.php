2f512df64959b487bd9c81df0265c6a03b041272
earlgray283 <earlgray283@gmail.com>
2020-01-13 00:27:01 +0900

    change id,ps

--- php/db_connect.php
+++ php/db_connect.php
@@ -5,14 +5,14 @@ error_reporting(E_ALL);
 
 class DBC{
     private $dsn;
-    private $user = 'earlgray283';
-    private $password = 'z76543215911';
+    private $user;
+    private $password;
 
     function __construct(){
         try {
             $this->dsn = 'mysql:dbname=cafecoder;host=localhost;charset=utf8;';
-            $this->user = 'earlgray283';
-            $this->password = 'z76543215911';
+            $this->user = 'ggggcxquicimfh';
+            $this->password = '74506a7ee5be1314f0de64c37ab527ebcb91ee31e733949c341c2a5555572e2f';
             $this->dbh = new PDO($this->dsn, $this->user, $this->password);
             //echo 'Success!!';
         } catch (PDOException $e) {
